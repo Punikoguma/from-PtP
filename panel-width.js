71f09950e228b1187d204b6c2ee7081726bd1dec
Punikoguma <ryotaroyamada0501@icloud.com>
2024-03-09 19:07:00 +0900

    n

--- panel-width.js
+++ panel-width.js
@@ -17,7 +17,18 @@ function updatePanelWidth() {//panel-leftの幅を決める関数（canvas-conta
     panelLeft.style.left = `${margin}px`;
     panelLeft.style.top = `${margin}px`;
 }
+// ウィンドウの高さを取得し、--vhカスタムプロパティに設定する関数
+function setViewportHeight() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+// 初期ロード時とウィンドウサイズ変更時に関数を実行
 
 // ウィンドウのリサイズ時とページの初期読み込み時に関数を実行
 window.addEventListener('resize', updatePanelWidth);
 document.addEventListener('DOMContentLoaded', updatePanelWidth);
+
+window.addEventListener('load', setViewportHeight);
+window.addEventListener('resize', setViewportHeight);
+
