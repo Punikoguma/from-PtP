ab4a0c651fac021a78416b1f1d565b13504cee33
Punikoguma <ryotaroyamada0501@icloud.com>
2024-03-10 00:35:51 +0900

    b

--- panel-width.js
+++ panel-width.js
@@ -10,7 +10,7 @@ function updatePanelWidth() {
     // panel-leftのleftプロパティを更新して、左側の余白がないことを保証
     panelLeft.style.left = `0px`; // 明示的に0pxとする
 }
-// ウィンドウの高さを取得し、--vhカスタムプロパティに設定する関数
+// ウィンドウの高さを取得し、--vhカスタムプロパティに設定する関数　あっささ
 function setViewportHeight() {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
