06818425b2832a61ffe649decfe25ae19a3f441a
Punikoguma <ryotaroyamada0501@icloud.com>
2024-03-10 17:55:53 +0900

    s

--- screen-check.js
+++ screen-check.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     function checkScreenAndToggleContent() {
         var content = document.querySelector('.content');
         var mobileWarning = document.querySelector('.mobile-warning');
-        var tabletLandscapeRecommendation = document.querySelector('.tablet-landscape-recommendation'); // 追加
+        var tabletLandscapeRecommendation = document.querySelector('.tablet-landscape-recommendation');
         
         // タブレットとスマホの判定
         var isTablet = window.innerWidth > 600 && window.innerHeight > 800;//タブレットの大きさ
@@ -12,17 +12,17 @@ document.addEventListener("DOMContentLoaded", function() {
         if (isMobile) {
             content.style.display = 'none';
             mobileWarning.style.display = 'block';
-            tabletLandscapeRecommendation.style.display = 'none'; // 追加
+            tabletLandscapeRecommendation.style.display = 'none';
         } 
         // タブレットで縦向きの場合
         else if (isTablet && window.matchMedia("(orientation: portrait)").matches) {
             content.style.display = 'none';
-            mobileWarning.style.display = 'none'; // 修正
-            tabletLandscapeRecommendation.style.display = 'block'; // 追加
+            mobileWarning.style.display = 'none';
+            tabletLandscapeRecommendation.style.display = 'block';
         } else {
             content.style.display = 'block';
             mobileWarning.style.display = 'none';
-            tabletLandscapeRecommendation.style.display = 'none'; // 追加
+            tabletLandscapeRecommendation.style.display = 'none';
         }
     }
 
@@ -30,14 +30,3 @@ document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener('resize', checkScreenAndToggleContent);
 });
 
-// 画面の向きが変わった場合の処理はそのまま利用可能
-let isPortrait = (window.innerHeight > window.innerWidth);
-
-window.addEventListener('resize', function() {
-    let wasPortrait = isPortrait;
-    isPortrait = (window.innerHeight > window.innerWidth);
-
-    if (isPortrait !== wasPortrait) {
-        window.location.reload();
-    }
-});
