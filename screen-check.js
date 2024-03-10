fb4c35c09a0749e40910e497620bad31ddab9276
Punikoguma <ryotaroyamada0501@icloud.com>
2024-03-10 15:18:48 +0900

    a

--- screen-check.js
+++ screen-check.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     function checkScreenAndToggleContent() {
       var content = document.querySelector('.content');
       var mobileWarning = document.querySelector('.mobile-warning');
-      if (window.innerWidth <= 10000 || (window.innerHeight <= 10000 && window.matchMedia("(orientation: landscape)").matches)) {
+      if (window.innerWidth <= 600 || (window.innerHeight <= 600 && window.matchMedia("(orientation: landscape)").matches)) {
         content.style.display = 'none';
         mobileWarning.style.display = 'block'; // .contentが非表示の時、.mobile-warningを表示
       } else {
